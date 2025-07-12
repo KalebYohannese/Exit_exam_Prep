8ff64bb7049a0d787d13052e07a2167dbe97f6a6
Kaleb Yohannese Tewelde <125556510+wolfgang-di-medicci@users.noreply.github.com>
2025-07-12 16:08:06 +0300

    Created folder structure

---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white px-4 py-3 flex gap-6">
+      <Link href="/">Home</Link>
+      <Link href="/notes">Notes</Link>
+      <Link href="/plan">Plan</Link>
+      <Link href="/exam">Exam</Link>
+    </nav>
+  );
+}
