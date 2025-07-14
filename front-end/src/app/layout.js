ce8fafd7f7644cebb5091ad94f4ea78c5c932859
Kaleb Yohannese Tewelde <125556510+wolfgang-di-medicci@users.noreply.github.com>
2025-07-14 20:16:29 +0300

    Minor changes

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -1,16 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/Navbar";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import Navbar from "./components/Navbar";
 
 export const metadata = {
   title: "Exam Prep",
@@ -20,10 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        className="bg-gray-50 text-gray-900"
-      >
+      <body className="bg-gray-50 text-gray-900">
         <Navbar />
         <main className="p-6 max-w-4xl mx-auto">{children}</main>
       </body>
