40ce5eb20583f3119208248d96199a334fe61355
Kaleb Yohannese Tewelde <125556510+wolfgang-di-medicci@users.noreply.github.com>
2025-07-18 12:58:34 +0300

    tailwind modifications

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <h1>
         Get Answers For The Ethiopian Exit Exam Questions With The Help Of AI.
       </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <Card title="Accounting" />
         <Card title="Animal-Science" />
