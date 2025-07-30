6025620bc98d5e55bd40ab5abb99095eacb0a73b
Kaleb Yohannese Tewelde <125556510+wolfgang-di-medicci@users.noreply.github.com>
2025-07-30 11:50:42 +0300

    started dynamic routing

--- front-end/src/app/_components/Card.js
+++ front-end/src/app/_components/Card.js
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import img from "/public/moe.png";
+import Link from "next/link";
 
 export default function Card({ title }) {
+  const id = 1;
+
   return (
     <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white">
-      <Image
-        src={img}
-        alt={title}
-        placeholder="blur"
-        className="w-full h-48 object-cover"
-      />
+      <Link href={`/exam/${id}`}>
+        <Image
+          src={img}
+          alt={title}
+          placeholder="blur"
+          className="w-full h-48 object-cover"
+        />
 
-      <div className="px-6 py-4">
-        <h3 className="font-bold text-xl mb-2">{title}</h3>
-      </div>
+        <div className="px-6 py-4">
+          <h3 className="font-bold text-xl mb-2">{title}</h3>
+        </div>
+      </Link>
     </div>
   );
 }
