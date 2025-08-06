01c5edef91ec7c025bb0d3038bc1854c5ca4e72f
Kaleb Yohannese Tewelde <125556510+wolfgang-di-medicci@users.noreply.github.com>
2025-08-06 21:49:07 +0300

    small modifications

--- back-end/scripts/seedQuestions.js
+++ back-end/scripts/seedQuestions.js
@@ -11,9 +11,9 @@ async function insertQuestions() {
   const { data, error } = await supabase.from("questions").insert(questions);
 
   if (error) {
-    console.error("❌ Error inserting questions:", error.message);
+    console.error("Error inserting questions:", error.message);
   } else {
-    console.log("✅ Successfully inserted questions!");
+    console.log("Successfully inserted questions!");
     console.log(data);
   }
 }
