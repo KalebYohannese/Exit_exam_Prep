7ee50cebd97d17a9edb12869d495110f25815af2
Kaleb Yohannese Tewelde <125556510+wolfgang-di-medicci@users.noreply.github.com>
2025-08-09 18:15:32 +0300

    finished uploading questions

--- back-end/data/questionsData.js
+++ back-end/data/questionsData.js
@@ -1,6 +1,7 @@
+// question format
+
 const questions = [
   {
-    id: 8,
     question: `Consider the following fragment of C++ code:
 
 const int x = 8;
@@ -20,6 +21,8 @@ For codes like the above one, which technique of code optimization is appropriat
     answer: 3,
     explanation:
       "Dead code elimination would remove the entire if block since the condition (8 > 16) is always false at compile time, making the code inside unreachable.",
+    course: 9,
+    year: 2015,
   },
 ];
 
