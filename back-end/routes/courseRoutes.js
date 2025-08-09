3c87f49f9eb717d6a85aceb6eb0b72f37ab44eb3
Kaleb Yohannese Tewelde <125556510+wolfgang-di-medicci@users.noreply.github.com>
2025-08-09 19:59:46 +0300

    back-end route modification

--- back-end/routes/courseRoutes.js
+++ back-end/routes/courseRoutes.js
@@ -2,19 +2,36 @@ const express = require("express");
 const router = express.Router();
 const supabase = require("../service/supabase");
 
-// GET /courses/
+// GET
+// /courses get all courses
 router.get("/", async (req, res) => {
   try {
     const { data, error } = await supabase.from("courses").select("*");
 
-    if (error) {
-      console.error("Supabase error:", error);
-      return res.status(500).json({ message: "Failed to fetch courses" });
-    }
+    if (error) throw error;
 
     res.json(data);
   } catch (err) {
-    console.error("Server error:", err);
+    console.error("Error fetching all courses", err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// /courses/courseId get specific course
+router.get("/:courseId", async (req, res) => {
+  try {
+    const { courseId } = req.params;
+
+    const { data, error } = await supabase
+      .from("courses")
+      .select("*")
+      .eq("id", Number(courseId));
+
+    if (error) throw error;
+
+    res.json(data);
+  } catch (err) {
+    console.error("Error fetching course", err);
     res.status(500).json({ message: "Server error" });
   }
 });
--- back-end/routes/questionRoutes.js
+++ back-end/routes/questionRoutes.js
@@ -2,19 +2,41 @@ const express = require("express");
 const router = express.Router();
 const supabase = require("../service/supabase");
 
-// GET /questions/
+// GET
+// /questions get all qeustions
 router.get("/", async (req, res) => {
   try {
-    const { data, error } = await supabase.from("questions").select("*");
+    const { data, error } = await supabase
+      .from("questions")
+      .select("*")
+      .order("id", { ascending: true });
 
-    if (error) {
-      console.error("Supabase error:", error);
-      return res.status(500).json({ message: "Failed to fetch questions" });
-    }
+    if (error) throw error;
 
     res.json(data);
   } catch (err) {
-    console.error("Server error:", err);
+    console.error("Error fetching all questions:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// /questions/courseId/year get specific questions
+router.get("/:courseId/:year", async (req, res) => {
+  try {
+    const { courseId, year } = req.params;
+
+    const { data, error } = await supabase
+      .from("questions")
+      .select("*")
+      .eq("course", Number(courseId))
+      .eq("year", Number(year))
+      .order("id", { ascending: true });
+
+    if (error) throw error;
+
+    res.json(data);
+  } catch (err) {
+    console.error("Error fetching filtered questions:", err);
     res.status(500).json({ message: "Server error" });
   }
 });
