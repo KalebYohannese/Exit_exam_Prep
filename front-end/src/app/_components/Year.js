5e7ab4e67832b4ca7dc17b87da802f62afcf52cb
Kaleb Yohannese Tewelde <125556510+wolfgang-di-medicci@users.noreply.github.com>
2025-08-11 15:34:13 +0300

    used useEffect to get questions

--- front-end/src/app/_components/Year.js
+++ front-end/src/app/_components/Year.js
@@ -1,14 +1,41 @@
 "use client";
 
 import Question from "./Question";
+import { getQuestions } from "@/lib/data-service";
+import { useEffect, useState } from "react";
 
-export default function Year({ course, questions }) {
+export default function Year({ course }) {
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [year, setYear] = useState(0);
+
+  useEffect(() => {
+    if (year > 0) {
+      async function loadQuestions() {
+        setLoading(true);
+        const data = await getQuestions(course[0].id, year);
+        setQuestions(data);
+        setLoading(false);
+      }
+      loadQuestions();
+    }
+  }, [course, year]);
+
   return (
-    <>
-      <h1 className="text-6xl font-bold text-center">
-        {course[0].title} Exit exam
-      </h1>
-      <Question questions={questions} />;
-    </>
+    <div>
+      {year === 0 ? (
+        <div className="w-full space-x-4 text-center">
+          <button onClick={() => setYear(2015)}>2015</button>
+          <button onClick={() => setYear(2016)}>2016</button>
+        </div>
+      ) : (
+        <>
+          <h1 className="text-6xl font-bold text-center">
+            {course[0].title} Exit exam
+          </h1>
+          {loading ? <p>Loading...</p> : <Question questions={questions} />}
+        </>
+      )}
+    </div>
   );
 }
